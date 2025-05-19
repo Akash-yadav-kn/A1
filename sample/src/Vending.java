b6352966398ce4a2c349586708b8e8d900d0f378
Akash-yadav-kn <akashyadavkn99@gmail.com>
2025-05-19 20:55:56 +0530

    year guessing

---
+++ sample/src/Vending.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class Vending {
+    public static void main(String[] args) {
+        Scanner b = new Scanner(System.in);
+        System.out.println("enter your choice ");
+        char choice = b.next().charAt(0);
+        switch (choice) {
+            case 'A':
+                System.out.println("chocolate");
+                break;
+            case 'B':
+                System.out.println("bubble gum");
+                break;
+            case 'C':
+                System.out.println("toy");
+                break;
+            case 'D':
+                System.out.println("juice");
+                break;
+            case 'E':
+                System.out.println("ice cream");
+                break;
+            case 'F':
+                System.out.println("chips");
+                break;
+            case 'G':
+                System.out.println("cold coffee");
+                break;
+            case 'H':
+                System.out.println("green tea");
+                break;
+            case 'I':
+                System.out.println("biscuit");
+                break;
+            case 'J':
+                System.out.println("apple");
+                break;
+            default:
+                System.out.println("invalid choice");
+
+
+
+
+
+
+
+
+
+        }
+
+    }
+}
