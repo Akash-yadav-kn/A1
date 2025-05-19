8855d3be5cf926b88fcccd11118b34942b850e99
Akash-yadav-kn <akashyadavkn99@gmail.com>
2025-05-19 21:02:51 +0530

    year guessing

---
+++ sample/src/Marks.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Marks {
+    public static void main(String[] args) {
+        Scanner b = new Scanner(System.in);
+        System.out.println("enter your choice ");
+        int choice = b.nextInt();
+        switch (choice) {
+            case 90:
+                System.out.println("A+");
+                break;
+            case 80:
+                System.out.println("B+");
+                break;
+            case 70:
+                System.out.println("C+");
+                break;
+            case 60:
+                System.out.println("D+");
+                break;
+            default:
+                System.out.println("fail");
+
+        }
+    }
+}
