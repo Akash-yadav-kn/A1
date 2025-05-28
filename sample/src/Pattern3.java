947ee1ebe74b8e9d2c93b2829ede97488931d9f1
Akash-yadav-kn <akashyadavkn99@gmail.com>
2025-05-28 21:01:37 +0530

    patter

---
+++ sample/src/Pattern3.java
@@ -0,0 +1,16 @@
+public class Pattern3 {
+
+        public static void main(String[] args) {
+            int i;
+            int j;
+            for(i=1;i<=5;i++){
+                for(j=1;j<=5;j++){
+                    System.out.print(i +" ");
+                }
+                System.out.println();
+            }
+
+        }
+    }
+
+
