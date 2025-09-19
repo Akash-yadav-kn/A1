86d035aa01e6f7bdbc11afac24eaf16c6a23ed96
Akash-yadav-kn <akashyadavkn99@gmail.com>
2025-09-19 11:35:18 +0530

    hero

---
+++ a1/src/Hero.java
@@ -0,0 +1,9 @@
+public class Hero {
+   int age=22;
+    public static void main(String[] args) {
+        String name="batman";
+        System.out.println("akash yadav is:"+name);
+        Hero akash=new Hero();
+        System.out.println("age:"+akash.age);
+    }
+}
