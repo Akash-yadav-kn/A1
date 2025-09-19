2cfdb21d7d23a3afb225f890da47d286145a0787
Akash-yadav-kn <akashyadavkn99@gmail.com>
2025-09-19 11:36:03 +0530

    123

---
+++ sample/src/Check6.java
@@ -0,0 +1,21 @@
+public class Check6 {
+    public static void main(String[] args) {
+        int i;
+        int j;
+        for (i = 1; i <= 5; i++)
+        for (j=1;j<=3;j++)
+        {
+            if (5 % i == 0) {
+                System.out.println(i + " is divide by 5");
+            } else if ( 3%j == 0) {
+                System.out.println(j+ " is  divide by 3");
+            }
+
+            {
+
+            }
+        }
+    }
+}
+
+
